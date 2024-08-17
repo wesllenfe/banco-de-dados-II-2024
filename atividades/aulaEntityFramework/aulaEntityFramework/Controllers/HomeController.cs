c6e0071acc4bbd0416d9947b9ab070818fe1f404
Wesllen Felipe <wesllenf11@gmail.com>
2024-08-16 21:43:40 -0300

    Configurado Entity Framework

---
+++ atividades/aulaEntityFramework/aulaEntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using aulaEntityFramework.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace aulaEntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        [HttpGet]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
