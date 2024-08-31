26e0946f29d460533cba5f888070bcc654d2e1e2
Wesllen Felipe <wesllenf11@gmail.com>
2024-08-30 21:26:52 -0300

    feat add crud adress

---
+++ atividades/aulaEntityFramework/aulaEntityFramework/Controllers/AdressesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using aulaEntityFramework.Models;
+
+namespace aulaEntityFramework.Controllers
+{
+    public class AdressesController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public AdressesController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Adresses
+        public async Task<IActionResult> Index()
+        {
+            var myDbContext = _context.Adresses.Include(a => a.Person);
+            return View(await myDbContext.ToListAsync());
+        }
+
+        // GET: Adresses/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var adress = await _context.Adresses
+                .Include(a => a.Person)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (adress == null)
+            {
+                return NotFound();
+            }
+
+            return View(adress);
+        }
+
+        // GET: Adresses/Create
+        public IActionResult Create()
+        {
+            ViewData["PersonID"] = new SelectList(_context.Persons, "Id", "Id");
+            return View();
+        }
+
+        // POST: Adresses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Street,Number,Neighborhood,City,UF,Country,CEP,PersonID")] Adress adress)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(adress);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PersonID"] = new SelectList(_context.Persons, "Id", "Id", adress.PersonID);
+            return View(adress);
+        }
+
+        // GET: Adresses/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var adress = await _context.Adresses.FindAsync(id);
+            if (adress == null)
+            {
+                return NotFound();
+            }
+            ViewData["PersonID"] = new SelectList(_context.Persons, "Id", "Id", adress.PersonID);
+            return View(adress);
+        }
+
+        // POST: Adresses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Street,Number,Neighborhood,City,UF,Country,CEP,PersonID")] Adress adress)
+        {
+            if (id != adress.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(adress);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AdressExists(adress.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PersonID"] = new SelectList(_context.Persons, "Id", "Id", adress.PersonID);
+            return View(adress);
+        }
+
+        // GET: Adresses/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var adress = await _context.Adresses
+                .Include(a => a.Person)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (adress == null)
+            {
+                return NotFound();
+            }
+
+            return View(adress);
+        }
+
+        // POST: Adresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var adress = await _context.Adresses.FindAsync(id);
+            if (adress != null)
+            {
+                _context.Adresses.Remove(adress);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AdressExists(long id)
+        {
+            return _context.Adresses.Any(e => e.Id == id);
+        }
+    }
+}
